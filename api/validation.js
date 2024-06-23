68ea9498b67bd9e12a36e5ffd816269b31511771
Aloyce CodeCraftsman <125455306+Allous-pun@users.noreply.github.com>
2024-06-23 16:25:54 +0300

    Update validation.js

--- api/validation.js
+++ api/validation.js
@@ -1,9 +1,4 @@
-const express = require('express');
-const router = express.Router();
-
-router.post('/', (req, res) => {
+module.exports = (req, res) => {
     console.log('Received validation request:', req.body);
     res.status(200).json({ message: 'Validation successful' });
-});
-
-module.exports = router;
+};
